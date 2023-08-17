b5cc615f2de710daf253bfdc5816ce6dcae64e67
lgaljer20 <lgaljer20@foi.hr>
2023-08-17 12:12:30 +0200

    Kreiran data transfer object (DTO) za AprsMessage

---
+++ cassiopeia-be.Business/DTO/AprsMessageDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace cassiopeia_be.Business.DTO
+{
+    public class AprsMessageDTO : BaseDTO
+    {
+        public DateTime Time { get; set; }
+        public string Message { get; set; }
+        public string Observer { get; set; }
+        public string Station { get; set; }
+        public string Source { get; set; }
+        public int SatelliteId { get; set; }
+    }
+}
