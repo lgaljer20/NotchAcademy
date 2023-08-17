bfc5e5f513472477e4d28c9293a1af24f59704aa
lgaljer20 <lgaljer20@foi.hr>
2023-08-17 12:05:16 +0200

    Implementiran entity AprsMessage

---
+++ cassiopeia-be.Data/Entities/AprsMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cassiopeia_be.Data.Entities
+{
+    public class AprsMessage : BaseEntity
+    {
+        public DateTime Time { get; set; }
+        public string Message { get; set; }
+        public string Observer { get; set; }
+        public string Station { get; set; }
+        public string Source { get; set; }
+
+        [ForeignKey(nameof(Satellite))]
+        public int SatelliteId { get; set; }
+        public virtual SatelliteInfo Satellite { get; set; }
+
+
+    }
+}
