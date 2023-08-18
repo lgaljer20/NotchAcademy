0ef9480aa1bb6d923bfc558e22ae96b8067f13a6
lgaljer20 <lgaljer20@foi.hr>
2023-08-18 09:30:07 +0200

    Migracije na lokalnoj bazi

--- cassiopeia-be.Data/Migrations/CassiopeiaContextModelSnapshot.cs
+++ cassiopeia-be.Data/Migrations/CassiopeiaContextModelSnapshot.cs
@@ -22,6 +22,95 @@ namespace cassiopeia_be.Data.Migrations
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
 
+            modelBuilder.Entity("cassiopeia_be.Data.Entities.AprsMessage", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+
+                    b.Property<string>("Message")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Observer")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<int>("SatelliteId")
+                        .HasColumnType("int");
+
+                    b.Property<string>("Source")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Station")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<DateTime>("Time")
+                        .HasColumnType("datetime2");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("SatelliteId");
+
+                    b.ToTable("AprsMessages");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            Message = "Hello from CubeSat-1!",
+                            Observer = "Ground Station A",
+                            SatelliteId = 1,
+                            Source = "NOCALL",
+                            Station = "CubeSat-1",
+                            Time = new DateTime(2023, 8, 17, 15, 54, 20, 853, DateTimeKind.Local).AddTicks(1855)
+                        },
+                        new
+                        {
+                            Id = 2,
+                            Message = "Temperature: 25°C, Battery: 85%",
+                            Observer = "Ground Station B",
+                            SatelliteId = 1,
+                            Source = "NOCALL",
+                            Station = "CubeSat-2",
+                            Time = new DateTime(2023, 8, 17, 15, 54, 20, 853, DateTimeKind.Local).AddTicks(1865)
+                        },
+                        new
+                        {
+                            Id = 3,
+                            Message = "Deployed solar panels successfully!",
+                            Observer = "Ground Station C",
+                            SatelliteId = 1,
+                            Source = "NOCALL",
+                            Station = "CubeSat-3",
+                            Time = new DateTime(2023, 8, 17, 15, 54, 20, 853, DateTimeKind.Local).AddTicks(1868)
+                        },
+                        new
+                        {
+                            Id = 4,
+                            Message = "Orbit update: Altitude: 400 km, Inclination: 45°",
+                            Observer = "Ground Station D",
+                            SatelliteId = 1,
+                            Source = "NOCALL",
+                            Station = "CubeSat-4",
+                            Time = new DateTime(2023, 8, 17, 15, 54, 20, 853, DateTimeKind.Local).AddTicks(1871)
+                        },
+                        new
+                        {
+                            Id = 5,
+                            Message = "Experiment results: Successful microgravity test.",
+                            Observer = "Ground Station E",
+                            SatelliteId = 1,
+                            Source = "NOCALL",
+                            Station = "CubeSat-5",
+                            Time = new DateTime(2023, 8, 17, 15, 54, 20, 853, DateTimeKind.Local).AddTicks(1873)
+                        });
+                });
+
             modelBuilder.Entity("cassiopeia_be.Data.Entities.SatelliteInfo", b =>
                 {
                     b.Property<int>("Id")
@@ -68,6 +157,17 @@ namespace cassiopeia_be.Data.Migrations
                             Owner = "notch",
                             Status = "active"
                         });
+                });
+
+            modelBuilder.Entity("cassiopeia_be.Data.Entities.AprsMessage", b =>
+                {
+                    b.HasOne("cassiopeia_be.Data.Entities.SatelliteInfo", "Satellite")
+                        .WithMany()
+                        .HasForeignKey("SatelliteId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Satellite");
                 });
 #pragma warning restore 612, 618
         }
--- cassiopeia-be.Data/cassiopeiaContext.cs
+++ cassiopeia-be.Data/cassiopeiaContext.cs
@@ -44,6 +44,46 @@ namespace cassiopeia_be.Data
                     Station = "CubeSat-1",
                     Source = "NOCALL",
                     SatelliteId = 1 //možda neće raditi
+                },
+                new AprsMessage
+                {
+                    Id = 2,
+                    Time = DateTime.Now,
+                    Message = "Temperature: 25°C, Battery: 85%",
+                    Observer = "Ground Station B",
+                    Station = "CubeSat-2",
+                    Source = "NOCALL",
+                    SatelliteId = 1
+                },
+                new AprsMessage
+                {
+                    Id = 3,
+                    Time = DateTime.Now,
+                    Message = "Deployed solar panels successfully!",
+                    Observer = "Ground Station C",
+                    Station = "CubeSat-3",
+                    Source = "NOCALL",
+                    SatelliteId = 1
+                },
+                new AprsMessage
+                {
+                    Id = 4,
+                    Time = DateTime.Now,
+                    Message = "Orbit update: Altitude: 400 km, Inclination: 45°",
+                    Observer = "Ground Station D",
+                    Station = "CubeSat-4",
+                    Source = "NOCALL",
+                    SatelliteId = 1
+                },
+                new AprsMessage
+                {
+                    Id = 5,
+                    Time = DateTime.Now,
+                    Message = "Experiment results: Successful microgravity test.",
+                    Observer = "Ground Station E",
+                    Station = "CubeSat-5",
+                    Source = "NOCALL",
+                    SatelliteId = 1
                 });
         }
     }
