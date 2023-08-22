61f4456fc0a29ce0198bd240b795f5e6071ac3f0
lgaljer20 <lgaljer20@foi.hr>
2023-08-22 14:19:29 +0200

    Inserted sql comands to fix updating database

--- cassiopeia-be.Data/Migrations/20230818171151_UpdateStatusField.cs
+++ cassiopeia-be.Data/Migrations/20230818171151_UpdateStatusField.cs
@@ -10,6 +10,11 @@ namespace cassiopeia_be.Data.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            
+            migrationBuilder.Sql("UPDATE SatelliteInfos SET Status = 1 WHERE Status = 'active';");
+            migrationBuilder.Sql("UPDATE SatelliteInfos SET Status = 0 WHERE Status = 'inactive';");
+
+
             migrationBuilder.AlterColumn<bool>(
                 name: "Status",
                 table: "SatelliteInfos",
