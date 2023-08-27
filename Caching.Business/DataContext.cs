99867683c6567c9e901fffcabf34e87c55cf9ed3
AmpaLampaX <aprotr01@fesb.hr>
2023-08-27 16:07:55 +0200

    add caching console app

---
+++ Caching.Business/DataContext.cs
@@ -0,0 +1,37 @@
+﻿using Caching.Business.Entities;
+using Caching.Business.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Xml.Schema;
+
+namespace Caching.Business
+{
+    public class DataContext : DbContext
+    {
+
+        public DbSet<SatelliteInfo> SatelliteInfo { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(XmlSchemaContentModel model)
+        {
+            model.Entity<SatelliteInfo>().HasData(
+                new SatelliteInfo
+                {
+                    id = 1,
+                    Name = "Satellite 1",
+                    Description = "Satellite 1 Description"
+                },
+                new SatelliteInfo
+                {
+                    id = 2,
+                    Name = "Satellite 2",
+                    Description = "Satellite 2 Description"
+                }
+                );
+        }
+
+    }
+}
